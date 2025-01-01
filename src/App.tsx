a5010303c59563c7f8f65b6caf44477a77ea883a
subas-roy <subasroy46@gmail.com>
2025-01-01 22:36:30 +0600

    Action Dispatch in Redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
+import { useDispatch } from "react-redux";
+import { decrement, increment } from "./redux/features/counter/counterSlice";
+
 function App() {
+  const dispatch = useDispatch();
+
+  const handleIncrement = () => {
+    dispatch(increment());
+  };
+
+  const handleDecrement = () => {
+    dispatch(decrement());
+  };
+
   return (
     <div>
       <h1>Counter with Redux</h1>
 
-      <button>Increment</button>
+      <button onClick={handleIncrement}>Increment</button>
       <div>0</div>
-      <button>Decrement</button>
+      <button onClick={handleDecrement}>Decrement</button>
     </div>
   );
 }
