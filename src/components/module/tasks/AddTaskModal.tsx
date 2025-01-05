24b6e0ac739a2ab6217a8d09e3ccb112fb449be7
subas-roy <subasroy46@gmail.com>
2025-01-05 09:55:04 +0600

    Create modal using shadcn

---
+++ src/components/module/tasks/AddTaskModal.tsx
@@ -0,0 +1,58 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useForm } from "react-hook-form";
+
+export function AddTaskModal() {
+  const form = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>Add Task</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Add Task</DialogTitle>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel />
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <DialogFooter>
+              <Button type="submit">Save changes</Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+}
--- src/pages/tasks.tsx
+++ src/pages/tasks.tsx
@@ -1,3 +1,4 @@
+import { AddTaskModal } from "@/components/module/tasks/AddTaskModal";
 import TaskCard from "@/components/module/tasks/TaskCard";
 import { selectTaks } from "@/redux/features/task/taskSlice";
 import { useAppSelector } from "@/redux/hook";
@@ -10,6 +11,7 @@ const Tasks = () => {
   return (
     <div className="mx-auto max-w-7xl px-5 mt-20">
       <h1>Tasks</h1>
+      <AddTaskModal />
       {tasks.map((task) => (
         <TaskCard task={task} />
       ))}
